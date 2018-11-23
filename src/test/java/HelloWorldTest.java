cf8a579ac59f2575e82b151af3599ae1aad196bf
AlexeySolovev <a-solovyov@i.ua>
2018-11-23 22:34:59 +0300

    Upd

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import java.util.Date;
@@ -10,21 +11,26 @@ public class HelloWorldTest  {
     public void initTest() {
          helloWorld = new HelloWorld();
     }
+    @After
+    public void afterTest() {
+        helloWorld = null;
+    }
+
     @Test
     public void sayHelloM() {
-        assertEquals(1,helloWorld.sayHello(new Date(1542994105837L-43200000L)));
+        assertEquals(4,helloWorld.sayHello(new Date(1542994105837L-43200000L)));
     }
 
     @Test
     public void sayHelloD() {
-        assertEquals(2,helloWorld.sayHello(new Date(1542994105837L-36000100L)));
+        assertEquals(1,helloWorld.sayHello(new Date(1542994105837L-36000100L)));
     }
     @Test
     public void sayHelloE() {
-        assertEquals(3,helloWorld.sayHello(new Date(1542994105837L)));
+        assertEquals(2,helloWorld.sayHello(new Date(1542994105837L)));
     }
     @Test
     public void sayHellon() {
-        assertEquals(4,helloWorld.sayHello(new Date(1542994105837L+360000000L)));
+        assertEquals(3,helloWorld.sayHello(new Date(1542994105837L+360000000L)));
     }
 }
