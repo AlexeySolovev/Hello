247f098636386f1f3b8745da99617b52e3b018f2
AlexeySolovev <a-solovyov@i.ua>
2018-11-23 22:17:44 +0300

    UPD

--- src/main/java/psvm.java
+++ src/main/java/psvm.java
@@ -1,8 +1,3 @@
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-import java.util.logging.LogManager;
-
 public class psvm {
     public static void main(String[] args)  {
         HelloWorld helloWorld = new HelloWorld();
